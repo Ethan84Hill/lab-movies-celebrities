dad39baf3397a71dfd0c0a09e203c693ea90b8da
Ethan <ethan84hill@gmail.com>
2022-11-14 12:55:22 -0500

    added folders and files

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
+const Celebs = require('./celebrities.routes')
+const Movies = require('./movies.routes')
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+
+
+
+
+
 
 module.exports = router;
